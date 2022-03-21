c0b6dca49b5a13e936cc0dd5df20f7cc91897a8c
Zion Yang <zionremnant@gmail.com>
2022-03-21 16:23:16 -0700

    TOC

--- index.js
+++ index.js
@@ -2,50 +2,43 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 inquirer
-    .prompt([
-        {
-        type: "input",
-        name: "title",
-        message: "What is your project title?",
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project title?",
     },
-        {type: "input",
-        name: "description",
-        message: "What is the description?",
-        },
-        {type: "list",
-        name: "license",
-        message: "What license are you using?",
-        choice: ["MIT", "Apache 2.0", "Hippocratice", "IBM"],
-        },
-        {type: "input",
-        name: "installation",
-        message: "What is the description?",
-        },
-        {type: "input",
-        name: "contributing",
-        message: "What is the description?",
-        },
-        {type: "input",
-        name: "usage",
-        message: "What is the description?",
-        },
-        {type: "input",
-        name: "test",
-        message: "What is the description?",
-        },
-        {type: "input",
-        name: "contact",
-        message: "What is your GitHub username?",
-        },
-    ]);
-    .then((data) => {
-// create readme
-const readme = 
-
-`# ${data.title}
+    { type: "input", name: "description", message: "What is the description?" },
+    {
+      type: "list",
+      name: "license",
+      message: "What license are you using?",
+      choices: ["MIT", "Apache 2.0", "Hippocratice", "IBM"],
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What is the installation?",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "What is contributing?",
+    },
+    { type: "input", name: "usage", message: "What is the usage?" },
+    { type: "input", name: "test", message: "What are you testing?" },
+    {
+      type: "input",
+      name: "contact",
+      message: "What is your GitHub username?",
+    },
+  ])
+  .then((data) => {
+    // create readme
+    const readme = `# ${data.title}
 
 ## Table of Contents
-* [Description] (#description)
+* [Description](#description)
 * [License](#license)
 * [Installation](#installation)
 * [Contributing](#contributing)
@@ -54,7 +47,7 @@ const readme =
 * [Contact](#contact)
 
 ## Description
-${data.decription}
+${data.description}
 
 ## License
 ${data.license}
@@ -73,8 +66,8 @@ ${data.test}
 
 ## Contact
 ${data.contact}
-`
-        fs.writeFile("readme.md", readme)
-    
-})
-}
+`;
+    fs.writeFile("readme.md", readme, (err) =>
+      err ? console.log(err) : console.log("Successful!")
+    );
+  });
---
+++ readme.md
@@ -0,0 +1,31 @@
+# readme
+
+## Table of Contents
+* [Description](#description)
+* [License](#license)
+* [Installation](#installation)
+* [Contributing](#contributing)
+* [Usage](#usage)
+* [Test](#test)
+* [Contact](#contact)
+
+## Description
+d
+
+## License
+MIT
+
+## Installation
+e
+
+## Contributing
+w
+
+## Usage
+q
+
+## Test
+v
+
+## Contact
+x
